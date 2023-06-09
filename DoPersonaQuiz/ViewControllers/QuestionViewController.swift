5b9cbd381e6aea1a193e3a879c32a5265362b364
Akhmedov96 <ahmedovnahid@icloud.com>
2023-06-09 10:21:18 +0500

    PersonalQuiz

--- DoPersonaQuiz/ViewControllers/QuestionViewController.swift
+++ DoPersonaQuiz/ViewControllers/QuestionViewController.swift
@@ -41,6 +41,11 @@ final class QuestionViewController: UIViewController {
         rangedSlider.value = answerCount / 2
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let resultVC = segue.destination as? ResultViewController else { return }
+        resultVC.answers = answersChosen
+    }
+    
     // MARK: - IB Actions
     @IBAction func singleButtonAnswerPressed(_ sender: UIButton) {
         guard let buttonIndex = singleButtons.firstIndex(of: sender) else { return }
@@ -58,14 +63,14 @@ final class QuestionViewController: UIViewController {
         nextQuestion()
     }
     
-    deinit {
-        print("\(type(of: self)) has been deallpcated")
-    }
-
     @IBAction func rangedButtonAnswerPressed(_ sender: Any) {
         let index = lrintf(rangedSlider.value)
         answersChosen.append(currentAnswers[index])
         nextQuestion()
+    }
+    
+    deinit {
+        print("\(type(of: self)) has been deallpcated")
     }
 }
 
--- DoPersonaQuiz/ViewControllers/ResultViewController.swift
+++ DoPersonaQuiz/ViewControllers/ResultViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 final class ResultViewController: UIViewController {
 
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var explanationResultLabel: UILabel!
+    
+    var answers: [Answer]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.hidesBackButton = true
+        let time = ContinuousClock().measure {
+            updateResult()
+        }
+        print(time)
     }
     
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
@@ -19,5 +28,30 @@ final class ResultViewController: UIViewController {
     }
     deinit {
         print("\(type(of: self)) has been deallpcated")
+    }
+}
+extension ResultViewController {
+    private func updateResult() {
+        var frequencyOfAnimals: [Animal: Int] = [:]
+        let animals = answers.map { $0.animal }
+        
+        for animal in animals {
+            if let animalTypeCount = frequencyOfAnimals[animal] {
+                frequencyOfAnimals.updateValue(animalTypeCount + 1, forKey: animal)
+            } else {
+                frequencyOfAnimals[animal] = 1
+            }
+        }
+        
+        let sortedFrequentOfAnimals = frequencyOfAnimals.sorted { $0.value > $1.value }
+        guard let mostFrequentAnimal = sortedFrequentOfAnimals.first?.key else { return }
+        
+        updateUI(with: mostFrequentAnimal)
+        
+    }
+    
+    private func updateUI(with animal: Animal) {
+        resultLabel.text = "Вы - \(animal.rawValue)!"
+        explanationResultLabel.text = animal.definition
     }
 }
